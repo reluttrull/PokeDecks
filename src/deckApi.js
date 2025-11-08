e1904ddddae5892bd658150b5238e851d9bab8c5
Ryan Luttrull <reluttrull@gmail.com>
2025-11-08 14:26:58 -0600

    update deck briefs endpoint

--- src/deckApi.js
+++ src/deckApi.js
@@ -2,7 +2,7 @@
 const BASE = "https://pokeserver20251017181703-ace0bbard6a0cfas.canadacentral-01.azurewebsites.net/deck";
 
 export const apiGetAllDeckBriefs = (setDeckBriefs) =>
-  fetch(`${BASE}/getalldeckbriefs`)
+  fetch(`${BASE}/getpublicdeckbriefs`)
     .then((response) => response.json())
     .then((data) => {
       setDeckBriefs(data);
