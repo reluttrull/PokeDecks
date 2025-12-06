4ac32dfc900a34f934c7a93c5e96f53d9bc9fd17
Ryan Luttrull <reluttrull@gmail.com>
2025-12-06 13:03:27 -0600

    Add weakness and resistance info to active pokemon stats

--- src/ActivePokemonStats.jsx
+++ src/ActivePokemonStats.jsx
@@ -6,6 +6,16 @@ const ActivePokemonStats = ({ active }) => {
     active && <div id="active-pokemon-stats">
         <div><em>{active.name}</em></div>
         <div>{`${active.hp - (active.damageCounters * 10)}/${active.hp} HP`}</div>
+        <ul>Weakness: {active.weaknesses && active.weaknesses.map(w => 
+            <li>
+                <img src={`/pokeclient/${w.type}.png`} style={{height: '15px', width: '15px'}} /> {w.value}
+            </li>)}
+        </ul>
+        <ul>Resistance: {active.resistances && active.resistances.map(r => 
+            <li>
+                <img src={`/pokeclient/${r.type}.png`} style={{height: '15px', width: '15px'}} /> {r.value}
+            </li>)}
+        </ul>
     </div>
 )};
 
